c23fb83c1b603d499ddb0d548675f7be31220d11
bagasys <37441075+bagasys@users.noreply.github.com>
2020-07-08 13:40:57 +0700

    implementing basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
 function App() {
   return (
     <GithubState>
       <AlertState>
-        hi
+        <Router>
+          <Switch>
+          <Route exact path="/" component={() => <div>Home</div>}/>
+          <Route exact path="/about" component={() => <div>About</div>}/>
+          <Route exact path="/users" component={() => <div>Users</div>}/>
+          <Route exact path="/users:login" component={() => <div>Detail Users</div>}/>
+          </Switch>
+        </Router>
       </AlertState>
     </GithubState>
   );
